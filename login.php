41021a56d14bfd5812c9791db5184098f0b564f0
LumiNovryM <luminovrymekel@gmail.com>
2022-10-04 13:27:06 +0700

    Membuat ERROR MESSAGE pada login

--- login.php
+++ login.php
@@ -1,12 +1,39 @@
+<?php
+
+include 'connect.php';
+
+    if(isset($_POST["login"])) {
+
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
 
+        $result = mysqli_query($connect, "SELECT * FROM akun WHERE email = '$email'");
+
+        // Cek email
+        if( mysqli_num_rows($result) === 1 ) {
+
+
+            // Cek Password
+            $row = mysqli_fetch_assoc($result);
+            if( password_verify($password, $row["password"]) ) {
+                header("Location: dashboard.php");
+            }
+
+        }
+
+        $error = true;
+
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style/login.css">
 </head>
 <body>
@@ -25,6 +52,10 @@
         <h1><span class="text-login">Login Member</span><span class="titik">.</span></h1>
         <p>Don't have account? <a href="register.php"><span class="biru"> Register </span></a></p>
     </div>
+    <!-- ERROR MESSAGE -->
+    <?php if(isset($error)) : ?>
+            <p class="error" style="color: #A80000;font-size: 18px;margin-left: 115px;margin-top: 50px;">Wrong Email & Password</p>
+    <?php  endif;?>
         <div class="form">
             <form action="" method="POST">
                     <div class="email">
@@ -35,7 +66,7 @@
                         <label for="password">Password</label>
                         <input type="password" name="password" id="password" required>
                     </div>
-                    <button type="submit" name="register">Login</button>
+                    <button type="submit" name="login">Login</button>
             </form>
         </div>
 
